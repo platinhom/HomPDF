0f9bb6e4e5b2723c5507bec50e5991e92a316da6
platinhom <zhaozxcpu@hotmail.com>
2015-07-08 18:30:28 +0800

    regular

--- gitsubmit.sh
+++ gitsubmit.sh
@@ -5,6 +5,6 @@ git add -A
 git commit -am "regular"
 if [ ! -z $1 ];then
 	#git remote add gitcafe git@gitcafe.com:platinhom/platinhom.git 
-	git push gitcafe master:gitcafe-pages
+	#git push gitcafe master:gitcafe-pages
 fi
-git push origin master
+git push origin gh-pages
